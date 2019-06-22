1da8de5826c8bd213c71cc25e57eed244da29c1c
Mathias Schrooten <mathiasschrooten@gmail.com>
2019-06-22 21:53:16 +0200

    ffs2

--- app.js
+++ app.js
@@ -34,6 +34,16 @@ app.get('/test', function(req, res) {
 });
 
 app.post('/kor/getBotResponse', async function(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', 'https://mathiasschrooten.github.io/dialogflow-web-v2/');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
 
     let text = req.body.text;
     let projectId = 'imc-axsbio';
